db75005c977657cac25dca9bce41687656961101
Odigietony <odigietony.jr@gmail.com>
2020-02-14 01:37:16 +0100

    Add project files.

---
+++ ParkyApi/Data/ApplicationDContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkyApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyApi.Data
+{
+    public class ApplicationDContext : DbContext
+    {
+        public ApplicationDContext(DbContextOptions<ApplicationDContext> options) : base(options)
+        {
+        }
+        DbSet<NationalPark> NationalParks { get; set; }
+    }
+}
