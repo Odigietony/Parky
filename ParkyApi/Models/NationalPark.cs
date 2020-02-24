11b3d3d89854505023f34e9926011a8376bfc0b0
Odigietony <odigietony.jr@gmail.com>
2020-02-24 12:22:57 +0100

    Add Image property in byte  array to National Park and National park DTo

--- ParkyApi/Models/NationalPark.cs
+++ ParkyApi/Models/NationalPark.cs
@@ -15,6 +15,7 @@ namespace ParkyApi.Models
         public string Name { get; set; }
         [Required]
         public string State { get; set; }
+        public byte[] Picture { get; set; }
         public DateTime Created { get; set; }
         public DateTime Established { get; set; }
     }
